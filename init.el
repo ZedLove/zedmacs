1aa144e4f9daa1e3f150329bc353f2a9ceb472a7
Zak Nitsch <zaknitsch@ZakBook-Pro.local>
2017-01-21 13:41:33 +0000

    add some helm customization, sudo-edit

--- init.el
+++ init.el
@@ -9,29 +9,27 @@
 (blink-cursor-mode -1)
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(global-visual-line-mode t)
+(global-visual-line-mode -1)
 (global-hl-line-mode 1)
 (line-number-mode t)
 (column-number-mode t)
 (size-indication-mode t)
-(linum-mode t)
+(linum-mode -1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Fonts
 (set-face-font 'default "-adobe-Source Code Pro-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1")
-
+(set-face-attribute 'default nil :height 135)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Mac setup
 ;; TODO : use this?
-(when (string= system-name "keeds-mbp.local")
-  (setenv "PATH" (concat (getenv "PATH") ":/Users/keeds/bin"))
-  (setq exec-path (append exec-path '("/Users/keeds/bin")))
-  (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-  (setq exec-path (append exec-path '("/usr/local/bin")))
-  (setenv "PATH" (concat (getenv "PATH") ":/Users/keeds/.nvm/versions/node/v4.4.7/bin"))
-  (setq exec-path (append exec-path '("/Users/keeds/.nvm/versions/node/v4.4.7/bin")))
-  (set-face-attribute 'default nil :height 140))
+;; (when (string= system-name "keeds-mbp.local")
+;;   (setenv "PATH" (concat (getenv "PATH") ":/Users/keeds/bin"))
+;;   (setq exec-path (append exec-path '("/Users/keeds/bin")))
+;;   (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+;;   (setq exec-path (append exec-path '("/usr/local/bin")))
+;;   (set-face-attribute 'default nil :height 140))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -69,6 +67,19 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; General Packages
 
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+  
+(use-package zenburn-theme
+  :ensure t
+  :init
+  (load-theme 'zenburn t))
+
 (use-package company
   :ensure t
   :config
@@ -80,13 +91,34 @@
   (autoload 'enable-paredit-mode "paredit" "" t))
 
 (use-package helm
-  :ensure t
+  :ensure t   
   :init
-  (bind-key "C-x b" 'helm-buffers-list)
-  (bind-key "C-x C-b" 'helm-buffers-list))
+  (require 'helm-config)
+  (helm-mode 1)
+  (helm-autoresize-mode 1)
+  (helm-push-mark-mode 1)
+  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+  (setq helm-scroll-amount 4
+	helm-ff-smart-completion t
+	helm-ff-skip-boring-files t)
+  :bind (("C-x C-f" . helm-find-files)
+	 ("C-x b" . helm-buffers-list)
+	 ("C-x C-b" . helm-buffers-list)
+	 ("M-x" . helm-M-x)
+	 ("M-y" . helm-show-kill-ring)))
 
 (use-package helm-ag
   :ensure t)
+
+(use-package helm-swoop
+  :ensure t
+  :init
+  (bind-key "M-i" 'helm-swoop))
+
+(use-package undo-tree
+  :ensure t
+  :init
+  (global-undo-tree-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ido
@@ -172,8 +204,28 @@
 (global-set-key (kbd "C--") 'text-scale-decrease)
 
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; custom settings
+;; Custom Fns
+(defun sudo-edit (&optional arg)
+  "Stolen from Andrew Keedle.
+   Edit currently visited file as root.
+   With a prefix ARG prompt for a file to visit.
+   Will also prompt for a file to visit if current
+   buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+(global-set-key (kbd "C-x C-r") 'sudo-edit)
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Custom
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -182,11 +234,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (elm-mode flycheck psc-ide auto-highlight-symbol less-css-mode use-package undo-tree rainbow-delimiters purescript-mode projectile paredit markdown-preview-mode markdown-mode+ magit highlight-parentheses helm-ag golden-ratio company-statistics cider))))
+    (try zenburn-theme zenburn helm-swoop elm-mode flycheck psc-ide auto-highlight-symbol less-css-mode use-package undo-tree rainbow-delimiters purescript-mode projectile paredit markdown-preview-mode markdown-mode+ magit highlight-parentheses helm-ag golden-ratio company-statistics cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
